08e060b1c3b9b4c200b4d1f3798b3e534ac5185d
Giltr749 <97340782+Giltr749@users.noreply.github.com>
2022-01-11 14:23:29 +0200

    updated pics

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-const GITHUB_URL = "https://api.github.com/users/IsraelTechChallengeITC";
+const GITHUB_URL = "https://api.github.com/users/Giltr749";
 
 fetch(GITHUB_URL)
   .then(function(response) {
@@ -18,7 +18,7 @@ const srcs = [
 
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27047.2349751655!2d34.80879391821973!3d32.07183511217572!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4c86e794e4ad%3A0x2d00bf3ee717526f!2sRamat%20Gan!5e0!3m2!1sen!2sil!4v1641900250684!5m2!1sen!2sil",
 
-  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54568.4348626958!2d34.75586759014464!3d31.261509922369484!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15026640029f8777%3A0x8dee8012deb5dd8!2sBe&#39;er%20Sheva!5e0!3m2!1sen!2sil!4v1641901759724!5m2!1sen!2sil",
+  "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54568.477284427034!2d34.755867587216265!3d31.26143655261696!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x15026640029f8777%3A0x8dee8012deb5dd8!2sBe&#39;er%20Sheva!5e0!3m2!1sen!2sil!4v1641903125590!5m2!1sen!2sil" ,
 
   "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d54084.90495522218!2d34.76222659367275!3d32.08799940299241!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x151d4ca6193b7c1f%3A0xc1fb72a2c0963f90!2sTel%20Aviv-Yafo!5e0!3m2!1sen!2sil!4v1641901959173!5m2!1sen!2sil",
 
