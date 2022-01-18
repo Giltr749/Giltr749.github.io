42908cb5772c7953befddfc9d2dd3e4e59259549
Giltr749 <97340782+Giltr749@users.noreply.github.com>
2022-01-18 12:04:58 +0200

    changed footer js

--- portfolio.js
+++ portfolio.js
@@ -10,21 +10,3 @@ fetch(GITHUB_URL)
     const myName = document.getElementById(`myName`);
     myName.textContent = data.login;
   });
-
-function $(x) {
-  return document.getElementById(x);
-}
-
-const langs = ["HTML", "CSS", "JavaScript"];
-let used = $(`built`).textContent;
-
-for(let i=0; i<=(langs.length-1); i++){
-  if (i === langs.length-1)
-    used += `and `;
-  used += (langs[i]+` `);;
-}
-
-$(`built`).textContent = used ;
-
-
-langs.join();
